bceac669bed1f211b6810d83df5d550b6531bc1f
dmitrii <dmitry-orlov@hotmail.com>
2019-03-13 09:39:30 +0100

    added ducks

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const records = require("./records.json");
 const countRecords = (records) => {
 	wolf_count = records.filter((r)=> r == "wolf").length;
 	sheep_count = records.filter((r)=> r == "sheep").length;
-        console.log(`wolves: ${wolf_count}, sheep: ${sheep_count}`);
+        duck_count = records.filter((r)=> r == "duck").length;
+	console.log(`wolves: ${wolf_count}, sheep: ${sheep_count}, duck: ${duck_count}`);
 }
 
 countRecords(records);
